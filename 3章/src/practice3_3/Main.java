1d2c211d2d8d7b442e012b08a3e386ea27138896
yonePC <ys.mailaddress0929@gmail.com>
2021-11-05 11:45:44 +0900

    practice3

---
+++ 3章/src/practice3_3/Main.java
@@ -0,0 +1,18 @@
+package practice3_3;
+
+public class Main {
+	public static void main(String[] args) {
+		int isHungry = 1;
+		String food = "ごはん";
+		System.out.println("こんにちは");
+		if (isHungry == 0) {
+			System.out.println("お腹がいっぱいです");
+		} else {
+			System.out.println("はらぺこです");
+		}
+		if (isHungry == 1) {
+			System.out.println(food + "をいただきます");
+		}
+		System.out.println("ごちそうさまでした");	
+	}
+}
---
+++ 3章/src/practice3_4/Main.java
@@ -0,0 +1,15 @@
+package practice3_4;
+
+public class Main {
+	public static void main(String[] args) {
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩にいきます");
+		} else {
+			System.out.println("DVDをみます");
+			System.out.println("寝ます");
+		}
+	}
+
+}
