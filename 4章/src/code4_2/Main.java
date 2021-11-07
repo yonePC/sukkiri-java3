5bf278937289fd288785d99e7606c084332b88c8
yonePC <ys.mailaddress0929@gmail.com>
2021-11-07 10:05:28 +0900

    4-2

---
+++ 4章/src/code4_2/Main.java
@@ -0,0 +1,14 @@
+package code4_2;
+
+public class Main {
+	public static void main (String[] args) {
+//		int[] scores;
+//		scores = new int [5];
+		
+		int[] scores = new int[5];
+		
+		int num = scores.length;
+		System.out.println("要素の数:" + num);
+	}
+
+}
---
+++ 4章/src/code4_7/Main.java
@@ -0,0 +1,9 @@
+package code4_7;
+
+public class Main {
+	public static void main(String[] arg) {
+		int[] scores = new int[5];
+		System.out.println(scores[0]);
+	}
+
+}
