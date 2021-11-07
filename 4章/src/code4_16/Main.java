e746dcc378f15e58c739aea1fa201defff42be2b
yonePC <ys.mailaddress0929@gmail.com>
2021-11-07 11:26:38 +0900

    4-6

---
+++ 4章/src/code4_16/Main.java
@@ -0,0 +1,13 @@
+package code4_16;
+
+public class Main {
+	public static void main(String[] args) {
+		boolean judge = true;
+		if (judge == true) {
+			int[] array = {1, 2, 3};
+			array = null;
+//			array[0] = 10;
+		}
+	}
+
+}
