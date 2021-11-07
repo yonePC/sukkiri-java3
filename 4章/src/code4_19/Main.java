53af875e19910b029b11226737ebbad3235c6619
yonePC <ys.mailaddress0929@gmail.com>
2021-11-07 11:35:41 +0900

    4-7

---
+++ 4章/src/code4_19/Main.java
@@ -0,0 +1,10 @@
+package code4_19;
+
+public class Main {
+	public static void main(String[] args) {
+		int[][] scores = {{40, 50, 60}, {80, 60, 70}};
+		System.out.println(scores.length);
+		System.out.println(scores[0].length);
+	}
+
+}
