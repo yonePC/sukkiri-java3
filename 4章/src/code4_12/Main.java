591dabd19209785520fe7fb7e9443e83b0d4f5ec
yonePC <ys.mailaddress0929@gmail.com>
2021-11-07 11:01:18 +0900

    4-4

---
+++ 4章/src/code4_12/Main.java
@@ -0,0 +1,29 @@
+package code4_12;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] seq = new int[10];
+		
+		for (int i = 0; i < seq.length; i++) {
+			seq[i] = new java.util.Random().nextInt(4);
+		}
+		
+		for (int i = 0; i < seq.length; i++) {
+			switch (seq[i]) {
+			case 0:
+				System.out.print("A ");
+				break;
+			case 1:
+				System.out.print("T ");
+				break;
+			case 2:
+				System.out.print("G ");
+				break;
+			case 3:
+				System.out.print("C ");
+				break;
+			}
+		}
+	}
+
+}
---
+++ 4章/src/code4_14/Main.java
@@ -0,0 +1,11 @@
+package code4_14;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] scores = {20, 30, 40, 50, 80 };
+		for(int value : scores) {
+			System.out.println(value);
+		}
+	}
+
+}
