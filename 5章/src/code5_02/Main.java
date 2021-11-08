b7ff5e08ed690ad0277de2e00f78c63605c583ae
yonePC <ys.mailaddress0929@gmail.com>
2021-11-08 10:53:21 +0900

    5-1

--- 5章/src/code5_02/Main.java
+++ 5章/src/code5_02/Main.java
@@ -1,4 +1,4 @@
-package code5_2;
+package code5_02;
 
 public class Main {
 	public static void main(String[] args) {
--- 5章/src/code5_03/Main.java
+++ 5章/src/code5_03/Main.java
@@ -1,4 +1,4 @@
-package code5_3;
+package code5_03;
 
 public class Main {
 	public static void methodA() {
