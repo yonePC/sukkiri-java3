063ea7429f1e9b690329feec8c3ef146b5444e45
yonePC <ys.mailaddress0929@gmail.com>
2021-11-14 11:29:22 +0900

    6-4

--- 6章/src/code6_05/calcapp/main/Calc.java
+++ 6章/src/code6_05/calcapp/main/Calc.java
@@ -1,5 +1,12 @@
 package code6_05.calcapp.main;
+import code6_06.calcapp.logics.CalcLogic;
 
 public class Calc {
+	public static void main(String[] args) {
+		int a  = 10; int b = 2;
+		int total = CalcLogic.tasu(a, b);
+		int delta = code6_06.calcapp.logics.CalcLogic.hiku(a, b);
+		System.out.println("足すと" + total + "引くと" + delta);
+	}
 
 }
--- 6章/src/code6_06/calcapp/logics/CalcLogic.java
+++ 6章/src/code6_06/calcapp/logics/CalcLogic.java
@@ -1,5 +1,11 @@
 package code6_06.calcapp.logics;
 
 public class CalcLogic {
-
+	public static int tasu(int a, int b) {
+		return(a + b);
+	}
+	
+	public static int hiku(int a, int b) {
+		return(a - b);
+	}
 }
