657204b10a81ec68c724ac9583cdf4b4e9ae1d3d
yonePC <ys.mailaddress0929@gmail.com>
2021-11-14 12:05:08 +0900

    practice6

---
+++ 6章/src/practice5_1/Main.java
@@ -0,0 +1,13 @@
+package practice5_1;
+import practice5_1.comment.Zenhan;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		practice5_1.comment.Kouhan.callDeae();
+		practice5_1.comment.Kouhan.showMondokoro();
+	}
+		
+}
+	
