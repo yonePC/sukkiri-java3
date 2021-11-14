7ea2959c75938387c1e86142f5267321fde46746
yonePC <ys.mailaddress0929@gmail.com>
2021-11-14 11:38:56 +0900

    6-6

---
+++ 6章/src/code6_10/Main.java
@@ -0,0 +1,12 @@
+package code6_10;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] heights = {172, 149, 152, 191, 155};
+		java.util.Arrays.sort(heights);
+		for (int h : heights) {
+			System.out.println(h);
+		}
+	}
+
+}
