ecedbdb913ef6904d8d63d8e3554cf013f3c331a
yonePC <ys.mailaddress0929@gmail.com>
2021-11-16 08:55:42 +0900

    9-3

---
+++ 9章/src/code9_08/Hero.java
@@ -0,0 +1,17 @@
+package code9_08;
+
+public class Hero {
+	String name;
+	int hp;
+	
+	public Hero(String name) {
+		this.hp = 100;
+		this.name = name;
+	}	
+		
+	public Hero() {
+		this.hp = 100;
+		this.name = "ダミー";
+	}
+
+}
---
+++ 9章/src/code9_08/Main.java
@@ -0,0 +1,13 @@
+package code9_08;
+
+public class Main {
+	public static void main(String[] args) {
+		Hero h1 = new Hero("ミナト");
+		System.out.println(h1.hp);
+		System.out.println(h1.name);
+		
+		Hero h2 = new Hero();
+		System.out.println(h2.name);
+	}
+
+}
