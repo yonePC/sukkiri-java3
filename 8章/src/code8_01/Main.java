dc2530995a59bcb07ba64448145710ee8bb31774
yonePC <ys.mailaddress0929@gmail.com>
2021-11-15 09:03:23 +0900

    8-2

---
+++ 8章/src/code8_01/Main.java
@@ -0,0 +1,8 @@
+package code8_01;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ 8章/src/code8_01/Matango.java
@@ -0,0 +1,7 @@
+package code8_01;
+
+public class Matango {
+	int hp;
+	final int LEVEL = 10;
+
+}
