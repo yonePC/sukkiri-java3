03024a20c4c9aaebc5d72550d0032590d1858eeb
yonePC <ys.mailaddress0929@gmail.com>
2021-11-15 09:22:36 +0900

    8-4

--- 8章/src/code8_01/Main.java
+++ 8章/src/code8_01/Main.java
@@ -2,7 +2,25 @@ package code8_01;
 
 public class Main {
 	public static void main(String[] args) {
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println("勇者" + h.name + "を生み出しました!");
 		
+		Matango m1 = new Matango();
+		m1.hp = 100;
+		m1.suffix = 'A';
+		
+		Matango m2 = new Matango();
+		m2.hp = 100;
+		m2.suffix = 'B';
+		
+		h.sit(5);
+		h.slip();
+		h.sit(25);
+		m1.run();
+		m2.run();
+		h.run();
 	}
 
 }
--- 8章/src/code8_01/Matango.java
+++ 8章/src/code8_01/Matango.java
@@ -3,5 +3,10 @@ package code8_01;
 public class Matango {
 	int hp;
 	final int LEVEL = 10;
+	char suffix;
+	
+	public void run() {
+		System.out.println("お化けキノコ" + this.suffix + "は逃げ出した！");
+	}
 
 }
