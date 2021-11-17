18dabcee08898b22d66413766090e91ef8094c23
yonePC <ys.mailaddress0929@gmail.com>
2021-11-17 15:28:06 +0900

    10-2

---
+++ 10章/src/chapter10_02/SuperHero.java
@@ -0,0 +1,27 @@
+package chapter10_02;
+
+public class SuperHero extends Hero {
+	boolean flying;
+	
+	public void fly() {
+		this.flying = true;
+		System.out.println("飛び上がった！");
+	}
+	
+	public void land() {
+		this.flying = false;
+		System.out.println("着地した！");
+	}
+	
+	public void run() {
+		System.out.println(this.name + "は撤退した！");
+	}
+	
+	public void attack(Matango m) {
+		super.attack(m);
+		if (this.flying) {
+			super.attack(m);
+		}
+	}
+
+}
