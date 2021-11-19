19ee4341704f8cbc597f0694de2a44555b054489
yonePC <ys.mailaddress0929@gmail.com>
2021-11-19 19:06:57 +0900

    11-3

---
+++ 11章/src/chapter01/Character.java
@@ -0,0 +1,17 @@
+package chapter01;
+
+public abstract class Character {
+	String name;
+	int hp;
+	
+	public void run() {
+		System.out.println(this.name + "は、逃げ出した！");
+	}
+	
+	public abstract void attack(Matango m);
+		///System.out.println(this.name + "の攻撃！");
+		///m.hp -= 
+		///System.out.println("敵にポイントのダメージを与えた！");		
+	
+
+}
