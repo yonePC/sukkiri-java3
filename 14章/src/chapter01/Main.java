3e234ad63c938c65f2d6c2f77cfa7879865fe012
yonePC <ys.mailaddress0929@gmail.com>
2021-11-27 13:53:25 +0900

    14-2

---
+++ 14章/src/chapter01/Main.java
@@ -0,0 +1,34 @@
+package chapter01;
+
+public class Main {
+	public static void main (String[] args) {
+		Empty e = new Empty();
+		String s = e.toString();
+		System.out.println(s);
+		
+		Object o1 = new Empty();
+		
+		Object o3 = "こんにちは"; 
+		
+		
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println(h.toString());
+		
+		
+		Hero h1 = new Hero();
+		h1.name = "ミナト";
+		h1.hp = 100;
+		
+		Hero h2 = new Hero();
+		h2.name = "ミナト";
+		h2.hp = 100;
+		
+		if(h1.equals(h2) == true) { 
+			System.out.println("同じ内容です");
+		} else {
+			System.out.println("違う内容です");
+		}
+	}
+}
