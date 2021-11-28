8e2827aa0be4f70cc274d04423dca1a16f0d7451
yonePC <ys.mailaddress0929@gmail.com>
2021-11-28 13:20:06 +0900

    14-3

---
+++ 14章/src/chapter02/Hero.java
@@ -0,0 +1,12 @@
+package chapter02;
+
+public class Hero {
+	String name;
+	int hp;
+	static int money;
+	
+	public static void setRandomMoney() {
+		Hero.money = (int)(Math.random() * 1000);
+	}
+
+}
