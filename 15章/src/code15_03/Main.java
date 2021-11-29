a393e81807a91720bf364818ee32a2bf7f362449
yonePC <ys.mailaddress0929@gmail.com>
2021-11-29 14:50:18 +0900

    15-2

---
+++ 15章/src/code15_03/Main.java
@@ -0,0 +1,10 @@
+package code15_03;
+
+public class Main {
+	public static void main(String[] args) {
+		String s1 = "Java progtamming";
+		System.out.println("文字列s1の4文字目以降は" + s1.substring(3));
+		System.out.println("文字列s1の4～8文字目は" + s1.substring(3,8));
+	}
+
+}
