211422c21e99edee2bcedd34cc6d5158d0076226
yonePC <ys.mailaddress0929@gmail.com>
2021-11-30 13:43:29 +0900

    15-5

--- 15章/src/code15_09/Main.java
+++ 15章/src/code15_09/Main.java
@@ -7,9 +7,18 @@ public class Main {
 		hero.job = "hero";
 		hero.gold = 280;
 		
+		Hero hero2 = new Hero();
+		hero2.name = "minatonZ";
+		hero2.job = "doragonmaster";
+		hero2.gold = 10000;
+				
+		
 		final String FORMAT = "%-9s %-13s 所持金%,6d";
 		String s = String.format(FORMAT, hero.getName(), hero.getJob(), hero.getGold());
 		System.out.println(s);
+		
+		String s2 = String.format(FORMAT, hero2.getName(), hero2.getJob(), hero2.getGold());
+		System.out.println(s2);
 	}
 
 }
