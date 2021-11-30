f61d30a6661920b746b304e4b1248a239fe2cf6d
yonePC <ys.mailaddress0929@gmail.com>
2021-11-30 13:41:55 +0900

    15-5

---
+++ 15章/src/code15_09/Hero.java
@@ -0,0 +1,20 @@
+package code15_09;
+
+public class Hero {
+	String name;
+	String job;
+	int gold;
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getJob() {
+		return this.job;
+	}
+	
+	public int getGold() {
+		return this.gold;
+	}
+
+}
