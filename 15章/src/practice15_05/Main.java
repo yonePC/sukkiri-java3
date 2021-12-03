393e9cc815aa6ac588473e298325ac834716785e
yonePC <ys.mailaddress0929@gmail.com>
2021-12-03 23:53:36 +0900

    practice15

--- 15章/src/practice15_05/Main.java
+++ 15章/src/practice15_05/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		LocalDate now = LocalDate.now();
 		LocalDate future = now.plusDays(100);
+//		LocalDate future = now.plus(Period.ofDays(100));		
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
 		System.out.println((fmt));
 	}
