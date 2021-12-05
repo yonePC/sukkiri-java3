f5e27c93192e2733b249fae7dc2edb19ec660c9b
yonePC <ys.mailaddress0929@gmail.com>
2021-12-05 12:10:43 +0900

    16-5

---
+++ 16章/src/code16_07/Main.java
@@ -0,0 +1,18 @@
+package code16_07;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	public static void main(String[] args) {
+		Map<String, Integer> prefs = new HashMap<String, Integer>();
+		prefs.put("京都府", 255);
+		prefs.put("東京都", 1261);
+		prefs.put("熊本県", 182);
+		for (String key : prefs.keySet()) {
+			int value = prefs.get(key);
+			System.out.println(key + "の人口は、" + value);
+		}
+	}
+
+}
