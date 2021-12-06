6bffe72d43bfd8b738c6ef367cc80ae8bde962b9
yonePC <ys.mailaddress0929@gmail.com>
2021-12-06 13:56:03 +0900

    practice16

---
+++ 16章/src/practice16/Hero.java
@@ -0,0 +1,8 @@
+package practice16;
+
+public class Hero {
+	private String name;
+	public Hero(String name) {this.name = name;}
+	public String getName() {return this.name;}
+
+}
---
+++ 16章/src/practice16/Main.java
@@ -0,0 +1,19 @@
+package practice16;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	public static void main(String[] args) {
+		Hero h1 = new Hero("斎藤");
+		Hero h2 = new Hero("鈴木");
+		Map<Hero, Integer> heros = new HashMap<Hero, Integer>();
+		heros.put(h1, 3);
+		heros.put(h2, 7);
+		for (Hero key : heros.keySet()) {
+			int value = heros.get(key);
+			System.out.println(key.getName() + "が倒した敵＝" + value);
+		}
+	}
+
+}
