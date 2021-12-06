b25bf914dfb1c55b4d98eb9cfb3fe7b8af5d34c7
yonePC <ys.mailaddress0929@gmail.com>
2021-12-06 13:44:54 +0900

    16-6

---
+++ 16章/src/code16_08/Main.java
@@ -0,0 +1,20 @@
+package code16_08;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Hero{
+	public String name;
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Hero h = new Hero();
+		h.name = "ミナト";
+		List<Hero> list = new ArrayList<Hero>();
+		list.add(h);
+		h.name = "スガワラ";
+		System.out.println(list.get(0).name);
+	}
+
+}
