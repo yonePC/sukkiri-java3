1403ecb5d4caff27ee72cf29cb497f65af3b6268
yonePC <ys.mailaddress0929@gmail.com>
2021-12-10 13:27:57 +0900

    18-3

---
+++ 18章/src/code18_04/Main.java
@@ -0,0 +1,16 @@
+package code18_04;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Class.forName("org.h2.Driver");
+		String dburl = "jdbc:h2:~/test";
+		String sql = "INSERT INTO EMPLOYEES(name) VALUES('aoki')";
+		Connection conn = DriverManager.getConnection(dburl);
+		conn.createStatement().executeUpdate(sql);
+		conn.close();
+	}
+
+}
